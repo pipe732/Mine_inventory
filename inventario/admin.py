b1d42613573935ca8ad12f8a6d79f1e6c15893b4
pipe732 <feliperodriguez080408@gmail.com>
2026-03-10 17:43:32 -0500

    feat: agregar el codigo de admin.py inventario

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import CategoriaHerramienta, Stock
 
+
+@admin.register(CategoriaHerramienta)
+class CategoriaHerramientaAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+    search_fields = ('nombre',)
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display  = ('codigo', 'herramienta', 'categoria', 'ubicacion', 'estado', 'fecha_ingreso')
+    list_filter   = ('estado', 'categoria')
+    search_fields = ('codigo', 'herramienta', 'observaciones')
+    readonly_fields = ('fecha_ingreso',)
+    ordering      = ('codigo',)
+    fieldsets = (
+        ('Identificación', {
+            'fields': ('codigo', 'herramienta', 'categoria')
+        }),
+        ('Detalles', {
+            'fields': ('ubicacion', 'estado', 'fecha_ingreso', 'observaciones')
+        }),
+    )
 # Register your models here.
